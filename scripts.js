2a3e9209b59c4233fae70612adf18c4f520a0ade
Wigwa <lucywigwa7@gmail.com>
2020-05-12 14:45:35 +0300

    hide function

--- scripts.js
+++ scripts.js
@@ -31,3 +31,58 @@ $(document).ready(function(){
       $(".managment").slideDown('1500');
     });
   });
+  $(document).ready(function(){
+    $(".wrk1").mouseover(function(){
+      $(".par").show();
+    }).mouseout(function(){
+      $(".par").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".wrk2").mouseover(function(){
+      $(".par2").show();
+    }).mouseout(function(){
+      $(".par2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".wrk3").mouseover(function(){
+      $(".par3").show();
+    }).mouseout(function(){
+      $(".par3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $(".wrk4").mouseover(function(){
+      $(".par4").show();
+    }).mouseout(function(){
+      $(".par4").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $(".wrk5").mouseover(function(){
+      $(".par5").show();
+    }).mouseout(function(){
+      $(".par5").hide();
+    });
+    $(".wrk6").mouseover(function(){
+      $(".par6").show();
+    }).mouseout(function(){
+      $(".par6").hide();
+    });
+    $(".wrk7").mouseover(function(){
+      $(".par7").show();
+    }).mouseout(function(){
+      $(".par7").hide();
+    });
+    $("wrk8").mouseover(function(){
+      $(".par8").show();
+    }).mouseout(function(){
+      $(".par8").hide();
+    });
+  });
+  
+  $(document).ready(function(){
+    $("form#form34A").submit(function(event){
+       event.preventDefault();
